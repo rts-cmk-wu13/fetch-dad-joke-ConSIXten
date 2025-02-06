a8ff6f74f181461a543f3fb2b3bfddac34e0ee40
conztantin <conztantinsixten@gmail.com>
2025-02-06 13:27:59 +0100

    dad jokes activeee

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+fetch("https://icanhazdadjoke.com/", {
+    headers:{
+        "Accept": "application/json"
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        let bodyElm = document.querySelector("body")
+
+        let divElm = document.querySelector(".div")
+        
+
+        divElm.innerHTML = `  
+            <h1>${data.joke}</h1>
+        `
+        bodyElm.append(divElm)
+
+    })
+
